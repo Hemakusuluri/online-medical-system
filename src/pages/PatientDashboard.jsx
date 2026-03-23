e5496740f71f9b9276223cc1976b9628d3d291fc
Hemakusuluri <2400031677@kluniversity.in>
2026-03-23 19:48:47 +0530

    Update PharmacistDashboard to display prescription data

--- src/pages/PatientDashboard.jsx
+++ src/pages/PatientDashboard.jsx
@@ -1,16 +1,24 @@
+import { useEffect, useState } from "react";
 import "./PharmacistDashboard.css";
 
 function PharmacistDashboard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("prescriptions")) || [];
+    setData(stored);
+  }, []);
+
   return (
     <div className="pharma-container">
 
       <h2>Pharmacy Portal</h2>
       <p className="subtitle">Manage and verify e-prescriptions</p>
 
-      {/* Top Cards */}
+      {/* Cards */}
       <div className="cards">
         <div className="card-box">
-          <h3 style={{ color: "blue" }}>0</h3>
+          <h3 style={{ color: "blue" }}>{data.length}</h3>
           <p>Pending Verification</p>
         </div>
 
@@ -20,40 +28,35 @@ function PharmacistDashboard() {
         </div>
 
         <div className="card-box">
-          <h3 style={{ color: "green" }}>2</h3>
+          <h3 style={{ color: "green" }}>0</h3>
           <p>Ready for Pickup</p>
         </div>
       </div>
 
-      {/* Prescription Queue */}
+      {/* Queue */}
       <div className="queue">
         <h3>E-Prescription Queue</h3>
 
-        <div className="queue-item">
-          <div className="left">
-            <div className="icon">📄</div>
-            <div>
-              <p><strong>hema</strong></p>
-              <span>dolo - 650</span>
-              <small>Dr. Sarah Chen</small>
+        {data.length === 0 ? (
+          <p>No prescriptions</p>
+        ) : (
+          data.map((item, index) => (
+            <div className="queue-item" key={index}>
+              <div className="left">
+                <div className="icon">📄</div>
+                <div>
+                  <p><strong>{item.patient}</strong></p>
+                  <span>{item.medicine}</span>
+                  <small>{item.dosage}</small>
+                </div>
+              </div>
+
+              <button className="ready-btn">
+                ✓ Ready for Pickup
+              </button>
             </div>
-          </div>
-
-          <button className="ready-btn">✓ Ready for Pickup</button>
-        </div>
-
-        <div className="queue-item">
-          <div className="left">
-            <div className="icon">📄</div>
-            <div>
-              <p><strong>Sri</strong></p>
-              <span>paracetamol - 500</span>
-              <small>Dr. Sarah Chen</small>
-            </div>
-          </div>
-
-          <button className="ready-btn">✓ Ready for Pickup</button>
-        </div>
+          ))
+        )}
 
       </div>
 
